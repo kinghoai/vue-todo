9532c6edc24540a47a41261fe418d3884340d8d9
Kinghoai <lamthanhhoai1990@gmail.com>
2019-08-31 10:25:44 +0700

    vue toto basic

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+.logo{
+  height: 70px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="todo">
+      {{ newTodo }}
+    <input type="text" placeholder="What need to be done?" class="todo__input" @keyup.enter="addTodo" v-model="newTodo"  />
+    <div class="todos" v-for="todo in todos" :key="todo.id">
+        {{ todo.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data() {
+    return {
+      newTodo: '',
+      todoId: 3,
+      todos: [
+        {
+          id: 1,
+          name: 'Quet Nha',
+          completed: true
+        },
+        {
+          id: 2,
+          name: 'Rua Chen',
+          completed: false
+        }
+      ]
+    };
+  },
+  methods: {
+      addTodo() {
+        var newTodo = {
+            id: this.todoId,
+            name: this.newTodo,
+            completed: false
+        };
+        this.todos.push(newTodo);
+        this.newTodo = '';
+        this.todoId++;
+      }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus">
+.todo {
+  background: #fafafa;
+  padding: 15px;
+
+  .todo__input {
+    padding: 5px;
+    width: 100%;
+    font-size: 16px;
+  }
+}
+</style>

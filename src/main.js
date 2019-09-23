ecf1cd1bd22aea14ed62d8a2de40e915b278733c
Kinghoai <lamthanhhoai1990@gmail.com>
2019-09-23 23:40:13 +0700

    npm i element-ui -S

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import Master from './components/layouts/Master.vue'
 import router from './router'
 import {store} from './store/store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 window.eventBus = new Vue()
-
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
